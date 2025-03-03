e7edff2c4d53445ef844debcc7dfe2f1f61b410c
Donato <hmkanggle@gmail.com>
2025-03-04 01:31:02 +0900

    update 0.3.18

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
+# v1.2.0
+
+##
+
+## 패키지 구성
+
+Release : https://github.com/Donato-touch/touch-of-animatediff/releases/tag/1.2.0
+
+사용법 : https://youtu.be/WMqAnL3y__c
+
+사용한 ComfyUI 버전
+- v0.3.18 : https://github.com/comfyanonymous/ComfyUI/releases/tag/v0.3.18
+
+사용한 Python 버전
+- 3.12.8
+
+설치되는 Python 패키지들 버전
+- https://github.com/Donato-touch/touch-of-animatediff/blob/1.2.0/comfy/requirements.txt
+
+이 버전으로 설치되는 Custom Nodes (고정 버전 링크) : 년-월-일
+- 이전과 동일한 것
+  - [ComfyUI_Comfyroll_CustomNodes](https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes/commit/d78b780ae43fcf8c6b7c6505e6ffb4584281ceca) : 2024-01-25
+  - [ComfyUI-Frame-Interpolation](https://github.com/Fannovel16/ComfyUI-Frame-Interpolation/commit/c336f7184cb1ac1243381e725fea1ad2c0a10c09) : 2024-10-11
+  - [efficiency-nodes-comfyui](https://github.com/jags111/efficiency-nodes-comfyui/commit/3ead4afd120833f3bffdefeca0d6545df8051798) : 2024-08-26
+  - [masquerade-nodes-comfyui](https://github.com/BadCafeCode/masquerade-nodes-comfyui/commit/432cb4d146a391b387a0cd25ace824328b5b61cf) : 2024-06-19
+  - [ComfyUI_FizzNodes](https://github.com/FizzleDorf/ComfyUI_FizzNodes/commit/7d6ea60c55ebd1268bd76fa462da052852bff192) : 2024-10-29
+  - [ComfyUI-Marigold](https://github.com/kijai/ComfyUI-Marigold/commit/1894ff2fd35c22f96c701067729237168b5f2ef5) : 2024-11-03
+  - [comfyui-art-venture](https://github.com/sipherxyz/comfyui-art-venture/commit/50abaace756b96f5f5dc2c9d72826ef371afd45e) : 2024-11-04
+  - [comfyui-upscale-by-model](https://github.com/TheBill2001/comfyui-upscale-by-model/commit/f8bb90094cb96c76e666d9eb609a09af9dcdfe95) : 2024-06-19
+  - [ComfyMath](https://github.com/evanspearman/ComfyMath/commit/939bb813f1c0ace959b62f20bb2da47190c4e211) : 2024-09-03
+  - [ComfyUI-SuperBeasts](https://github.com/SuperBeastsAI/ComfyUI-SuperBeasts/commit/f684c86d0031b82123dc1f7931a8de43c9946fee) : 2024-07-31
+  - [comfyui_segment_anything](https://github.com/storyicon/comfyui_segment_anything/commit/ab6395596399d5048639cdab7e44ec9fae857a93) : 2024-03-21
+  - [x-flux-comfyui](https://github.com/XLabs-AI/x-flux-comfyui/commit/00328556efc9472410d903639dc9e68a8471f7ac) : 2024-10-30
+  - [ComfyUI-Manager](https://github.com/ltdrdata/ComfyUI-Manager/releases/tag/2.55.5) : 2024-12-13
+  - [ComfyUI_essentials](https://github.com/cubiq/ComfyUI_essentials/commit/33ff89fd354d8ec3ab6affb605a79a931b445d99) : 2024-12-11
+
+- 업데이트됨
+  - [comfyui_controlnet_aux](https://github.com/Fannovel16/comfyui_controlnet_aux/commit/1e9eac6377c882da8bb360c7544607036904362c) : 2025-02-16
+  - [ComfyUI-Advanced-ControlNet](https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet/commit/b66cd70c9845a109a85b4a0ef13cefda41ca6039) : 2025-01-31
+  - [ComfyUI-AnimateDiff-Evolved](https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved/commit/94eb45621c7e5a5286968b3938b1b7689d34ced0) : 2025-01-31
+  - [ComfyUI-VideoHelperSuite](https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite/commit/4c7858ddd5126f7293dc3c9f6e0fc4c263cde079) : 2025-02-26
+  - [ComfyUI_IPAdapter_plus](https://github.com/cubiq/ComfyUI_IPAdapter_plus/commit/9d076a3df0d2763cef5510ec5ab807f6632c39f5) : 2025-02-26
+  - [ComfyUI-Impact-Pack](https://github.com/ltdrdata/ComfyUI-Impact-Pack/releases/tag/8.8.1) : 2025-02-02
+  - [ComfyUI-Impact-Subpack](https://github.com/ltdrdata/ComfyUI-Impact-Subpack/releases/tag/1.2.9) : 2025-01-25
+  - [ComfyUI-Inspire-Pack](https://github.com/ltdrdata/ComfyUI-Inspire-Pack/releases/tag/1.13) : 2025-01-31
+  - [ComfyUI-Custom-Scripts](https://github.com/pythongosssss/ComfyUI-Custom-Scripts/commit/bbda5e52ad580c13ceaa53136d9c2bed9137bd2e) : 2025-02-20
+  - [comfy_mtb](https://github.com/melMass/comfy_mtb/commit/d87e52ea2c112fd95f257dcd6a54a5db77a34fc3) : 2025-02-16
+  - [comfyui-dream-project](https://github.com/alt-key-project/comfyui-dream-project/commit/f48bed5b8ae866b3dad33fb811d712d45205f117) : 2025-02-16
+  - [ComfyUI-KJNodes](https://github.com/kijai/ComfyUI-KJNodes/commit/97d20e27e589854451a9d1f091f6524e947d6229) : 2025-02-27
+  - [ComfyUI-Florence2](https://github.com/kijai/ComfyUI-Florence2/commit/90b012e922f8bb0482bcd2ae24cdc191ec12a11f) : 2025-02-10
+  - [was-node-suite-comfyui](https://github.com/WASasquatch/was-node-suite-comfyui/commit/3ed45af34a14551dc28cb3127235cc7197d4633f) : 2025-02-15
+  - [rgthree-comfy](https://github.com/rgthree/rgthree-comfy/commit/32142fe476878a354dda6e2d4b5ea98960de3ced) : 2025-02-22
+  - [ComfyUI-Crystools](https://github.com/crystian/ComfyUI-Crystools/commit/72e2e9af4a6b9a58ca5d753cacff37ba1ff9bfa8) : 2025-02-03
+  - [ComfyUI_tinyterraNodes](https://github.com/TinyTerra/ComfyUI_tinyterraNodes/commit/b684adbcab271a2b7a3875bb97af27d495409a11) : 2025-02-17
+  - [ComfyUI_UltimateSDUpscale](https://github.com/ssitu/ComfyUI_UltimateSDUpscale/commit/1b6e60d4f1e7ef325ea4b6268f41c03bd18eee5b) : 2025-02-26
+  - [comfyui-various](https://github.com/jamesWalker55/comfyui-various/commit/5bd85aaf7616878471469c4ec7e11bbd0cef3bf2) : 2025-02-27
+  - [mikey_nodes](https://github.com/bash-j/mikey_nodes/commit/a8c52735f51be160f115ee4b9ccd0ed758d3b520) : 2025-02-22
+  - [ComfyUI-LogicUtils](https://github.com/aria1th/ComfyUI-LogicUtils/commit/2451f43fa7322b2d82cf61e74c51bb202acaa85c) : 2025-02-07
+
+- 추가됨
+  - [ComfyUI-HunyuanVideoWrapper](https://github.com/kijai/ComfyUI-HunyuanVideoWrapper/commit/2b043839ae7abe58ae8d3ea6099daea2b7f53d17) : 2025-03-02
+  - [cg-image-filter](https://github.com/chrisgoringe/cg-image-filter/commit/274896c27e7ca396546fa639ef5e9fbe3b2cbfbd) : 2025-02-26
+
+- 삭제됨
+  - [cg-image-picker](https://github.com/chrisgoringe/cg-image-picker/commit/aaab0d31165ee9cd252f62067de5f5e89240563e) : 2024-11-29
+  - [ComfyUI-LTXVideo](https://github.com/Lightricks/ComfyUI-LTXVideo/commit/4c5add5f4693e5bf55b58aad99326fab9e9b4a53) : 2024-12-22
+
+
+
+---
+
+
 # v1.1.4
 
 ## 추가된 Example
--- comfy/setup_custom_nodes.bat
+++ comfy/setup_custom_nodes.bat
@@ -39,6 +39,7 @@ md models\reactor
 md models\rembg
 md models\sams
 md models\style_models
+md models\text_encoders
 md models\ultralytics
 md models\unet
 md models\upscale_models
@@ -66,50 +67,46 @@ move run_nvidia_gpu_my.bat %COMFY_DIR%\
 
 %COMFY_DIR%\python_embeded\python.exe -m pip install --force-reinstall wheel
 %COMFY_DIR%\python_embeded\python.exe -m pip install -r requirements.txt
-%COMFY_DIR%\python_embeded\python.exe -m pip install --no-deps --force-reinstall "dghs-imgutils[gpu]" "pydantic>=2.7.0" "anyio" "httpcore==1.*" "matplotlib" "scikit-image" "timm==1.0.9" "pooch" "pymatting" "urllib3==1.26.20" "sympy==1.13.1" "Cython==3.0.11" "numpy<2"
+@REM %COMFY_DIR%\python_embeded\python.exe -m pip install --no-deps --force-reinstall "dghs-imgutils[gpu]" "pydantic>=2.7.0" "anyio" "httpcore==1.*" "matplotlib" "scikit-image" "timm==1.0.9" "pooch" "pymatting" "urllib3==1.26.20" "sympy==1.13.1" "Cython==3.0.11" "numpy<2"
 
 pushd %COMFY_DIR%\ComfyUI\custom_nodes
-git clone --recursive -b 2.55.5 https://github.com/ltdrdata/ComfyUI-Manager.git
+git clone --recursive -b 3.27.8 https://github.com/ltdrdata/ComfyUI-Manager.git
 call:cloneCustomNode "Suzie1" "ComfyUI_Comfyroll_CustomNodes" "d78b780"
-call:cloneCustomNode "Fannovel16" "comfyui_controlnet_aux" "5a049bd"
+call:cloneCustomNode "Fannovel16" "comfyui_controlnet_aux" "1e9eac6377c882da8bb360c7544607036904362c"
 call:cloneCustomNode "Fannovel16" "ComfyUI-Frame-Interpolation" "c336f71"
-call:cloneCustomNode "Kosinkadink" "ComfyUI-Advanced-ControlNet" "9632af9"
-call:cloneCustomNode "Kosinkadink" "ComfyUI-AnimateDiff-Evolved" "4f1344e"
-call:cloneCustomNode "Kosinkadink" "ComfyUI-VideoHelperSuite" "78753db"
-call:cloneCustomNode "cubiq" "ComfyUI_IPAdapter_plus" "b188a6c"
+call:cloneCustomNode "Kosinkadink" "ComfyUI-Advanced-ControlNet" "b66cd70c9845a109a85b4a0ef13cefda41ca6039"
+call:cloneCustomNode "Kosinkadink" "ComfyUI-AnimateDiff-Evolved" "94eb45621c7e5a5286968b3938b1b7689d34ced0"
+call:cloneCustomNode "Kosinkadink" "ComfyUI-VideoHelperSuite" "4c7858ddd5126f7293dc3c9f6e0fc4c263cde079"
+call:cloneCustomNode "cubiq" "ComfyUI_IPAdapter_plus" "9d076a3df0d2763cef5510ec5ab807f6632c39f5"
 call:cloneCustomNode "cubiq" "ComfyUI_essentials" "33ff89f"
-call:cloneCustomNodeByTag "ltdrdata" "ComfyUI-Impact-Pack" "8.0.1"
-call:cloneCustomNodeByTag "ltdrdata" "ComfyUI-Impact-Subpack" "1.1"
-call:cloneCustomNodeByTag "ltdrdata" "ComfyUI-Inspire-Pack" "1.9"
-call:cloneCustomNode "pythongosssss" "ComfyUI-Custom-Scripts" "19a82e2"
+call:cloneCustomNodeByTag "ltdrdata" "ComfyUI-Impact-Pack" "8.8.1"
+call:cloneCustomNodeByTag "ltdrdata" "ComfyUI-Impact-Subpack" "1.2.9"
+call:cloneCustomNodeByTag "ltdrdata" "ComfyUI-Inspire-Pack" "1.13"
+call:cloneCustomNode "pythongosssss" "ComfyUI-Custom-Scripts" "bbda5e52ad580c13ceaa53136d9c2bed9137bd2e"
 call:cloneCustomNode "jags111" "efficiency-nodes-comfyui" "3ead4af"
 call:cloneCustomNode "BadCafeCode" "masquerade-nodes-comfyui" "432cb4d"
-call:cloneCustomNode "melMass" "comfy_mtb" "827c64c"
+call:cloneCustomNode "melMass" "comfy_mtb" "d87e52ea2c112fd95f257dcd6a54a5db77a34fc3"
 call:cloneCustomNode "FizzleDorf" "ComfyUI_FizzNodes" "7d6ea60"
-call:cloneCustomNode "alt-key-project" "comfyui-dream-project" "b5c804a"
-call:cloneCustomNode "kijai" "ComfyUI-KJNodes" "973ceb6"
-call:cloneCustomNode "kijai" "ComfyUI-Florence2" "27714ba"
+call:cloneCustomNode "alt-key-project" "comfyui-dream-project" "f48bed5b8ae866b3dad33fb811d712d45205f117"
+call:cloneCustomNode "kijai" "ComfyUI-KJNodes" "97d20e27e589854451a9d1f091f6524e947d6229"
+call:cloneCustomNode "kijai" "ComfyUI-Florence2" "90b012e922f8bb0482bcd2ae24cdc191ec12a11f"
 call:cloneCustomNode "kijai" "ComfyUI-Marigold" "1894ff2"
-call:cloneCustomNode "WASasquatch" "was-node-suite-comfyui" "fe7e088"
+call:cloneCustomNode "kijai" "ComfyUI-HunyuanVideoWrapper" "2b043839ae7abe58ae8d3ea6099daea2b7f53d17"
+call:cloneCustomNode "WASasquatch" "was-node-suite-comfyui" "3ed45af34a14551dc28cb3127235cc7197d4633f"
 call:cloneCustomNode "sipherxyz" "comfyui-art-venture" "50abaac"
-call:cloneCustomNode "rgthree" "rgthree-comfy" "5f2d8a1"
-call:cloneCustomNode "chrisgoringe" "cg-image-picker" "aaab0d3"
-call:cloneCustomNode "crystian" "ComfyUI-Crystools" "03a61d6"
+call:cloneCustomNode "rgthree" "rgthree-comfy" "32142fe476878a354dda6e2d4b5ea98960de3ced"
+call:cloneCustomNode "chrisgoringe" "cg-image-filter" "274896c27e7ca396546fa639ef5e9fbe3b2cbfbd"
+call:cloneCustomNode "crystian" "ComfyUI-Crystools" "72e2e9af4a6b9a58ca5d753cacff37ba1ff9bfa8"
 call:cloneCustomNode "TheBill2001" "comfyui-upscale-by-model" "f8bb900"
-call:cloneCustomNode "TinyTerra" "ComfyUI_tinyterraNodes" "339ee9c"
-call:cloneCustomNode "ssitu" "ComfyUI_UltimateSDUpscale" "e617ff2"
+call:cloneCustomNode "TinyTerra" "ComfyUI_tinyterraNodes" "b684adbcab271a2b7a3875bb97af27d495409a11"
+call:cloneCustomNode "ssitu" "ComfyUI_UltimateSDUpscale" "1b6e60d4f1e7ef325ea4b6268f41c03bd18eee5b"
 call:cloneCustomNode "evanspearman" "ComfyMath" "939bb81"
-call:cloneCustomNode "jamesWalker55" "comfyui-various" "36454f9"
-call:cloneCustomNode "bash-j" "mikey_nodes" "637bc18"
+call:cloneCustomNode "jamesWalker55" "comfyui-various" "5bd85aaf7616878471469c4ec7e11bbd0cef3bf2"
+call:cloneCustomNode "bash-j" "mikey_nodes" "a8c52735f51be160f115ee4b9ccd0ed758d3b520"
 call:cloneCustomNode "SuperBeastsAI" "ComfyUI-SuperBeasts" "f684c86"
 call:cloneCustomNode "storyicon" "comfyui_segment_anything" "ab63955"
-call:cloneCustomNode "aria1th" "ComfyUI-LogicUtils" "eb3a0d0"
+call:cloneCustomNode "aria1th" "ComfyUI-LogicUtils" "2451f43fa7322b2d82cf61e74c51bb202acaa85c"
 call:cloneCustomNode "XLabs-AI" "x-flux-comfyui" "0032855"
-call:cloneCustomNode "Lightricks" "ComfyUI-LTXVideo" "8cbf26c"
-
-pushd ComfyUI-LTXVideo
-%COMFY_DIR%\python_embeded\python.exe -m pip install -r requirements.txt
-popd
 
 popd
 
